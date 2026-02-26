87278769751a55e18f62e90dbc99e08164433df9
Smu99le <45720974+Smu99le@users.noreply.github.com>
2026-02-26 10:14:45 +0200

    for excel

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from database.schema import create_tables
 from database.repository import insert_fish, fetch_history, update_fish
 from services.fish_service import calculate_total
 from services.report_service import summarize_by_client
+from io import BytesIO
 
 # --- Init ---
 conn = get_connection()
